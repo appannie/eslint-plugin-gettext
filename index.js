1c976641e096279fc2fa3033f4bce1c50f84326b
wxu <wxu@appannie.com>
2018-04-23 16:53:06 +0800

    Added rule for multiple variables

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 module.exports = {
     rules: {
         'no-variable-string': require('./lib/rules/no-variable-string'),
+        'required-positional-markers-for-multiple-variables': require('./lib/rules/required-positional-markers-for-multiple-variables'),
     },
     rulesConfig: {
         'no-variable-string': 0,
