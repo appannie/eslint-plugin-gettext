e496dc5711caf3275844c7712e9c2814d65ba682
John Vandenberg <jayvdb@gmail.com>
2021-10-02 01:54:57 +0800

    Add check for msgid in .po file (#22)
    Co-authored-by: John Vandenberg <john.vandenberg1@bhp.com>

---
+++ __tests__/lib/rules/has-translation-msgid.js
@@ -0,0 +1,113 @@
+'use strict';
+
+const ruleNoMissingMsgId = require('../../../lib/rules/has-translation-msgid');
+const RuleTester = require('eslint').RuleTester;
+
+const ruleTester = new RuleTester();
+const invalidMessage = 'String literal not in .po file.';
+
+ruleTester.run('has-translation-msgid', ruleNoMissingMsgId, {
+    valid: [
+        {
+            code: "gettext('hello')",
+            settings: { poFilename: '__tests__/hello.po' },
+        },
+        {
+            code: "ngettext('hello', 'hellos', 5)",
+            settings: { poFilename: '__tests__/hello.po' },
+        },
+        {
+            code: "pgettext('homepage', 'hi')",
+            settings: { poFilename: '__tests__/hello.po' },
+        },
+        {
+            code: "npgettext('homepage', 'hi', 'his', 5)",
+            settings: { poFilename: '__tests__/hello.po' },
+        },
+        // These are ignored as they are reported by no-variable-string
+        {
+            code: 'gettext(1)',
+            settings: { poFilename: '__tests__/hello.po' },
+        },
+        {
+            code: "ngettext(1, 'hellos', 5)",
+            settings: { poFilename: '__tests__/hello.po' },
+        },
+        {
+            code: "ngettext('hello', 1, 5)",
+            settings: { poFilename: '__tests__/hello.po' },
+        },
+        {
+            code: "pgettext(1, 'hi')",
+            settings: { poFilename: '__tests__/hello.po' },
+        },
+        {
+            code: "npgettext(1, 'hi', 'his', 5)",
+            settings: { poFilename: '__tests__/hello.po' },
+        },
+        {
+            code: "npgettext('homepage', 'hi', 1, 5)",
+            settings: { poFilename: '__tests__/hello.po' },
+        },
+    ],
+    invalid: [
+        {
+            code: "gettext('goodbye')",
+            settings: { poFilename: '__tests__/hello.po' },
+            errors: [
+                {
+                    message: invalidMessage,
+                    type: 'Literal',
+                },
+            ],
+        },
+        {
+            code: "ngettext('good', 'bye', 5)",
+            settings: { poFilename: '__tests__/hello.po' },
+            errors: [
+                {
+                    message: invalidMessage,
+                    type: 'Literal',
+                },
+                {
+                    message: invalidMessage,
+                    type: 'Literal',
+                },
+            ],
+        },
+        {
+            code: "ngettext('hello', 'bye', 5)",
+            settings: { poFilename: '__tests__/hello.po' },
+            errors: [
+                {
+                    message: invalidMessage,
+                    type: 'Literal',
+                },
+            ],
+        },
+        {
+            code: "pgettext('homepage', 'goodbye')",
+            settings: { poFilename: '__tests__/hello.po' },
+            errors: [
+                {
+                    message: invalidMessage,
+                    type: 'Literal',
+                },
+            ],
+        },
+        {
+            code: "npgettext('homepage', 'good', 'bye', 5)",
+            settings: { poFilename: '__tests__/hello.po' },
+            errors: [
+                {
+                    message: invalidMessage,
+                    type: 'Literal',
+                },
+                {
+                    message: invalidMessage,
+                    type: 'Literal',
+                },
+            ],
+        },
+    ],
+});
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
     rules: {
         'no-variable-string': require('./lib/rules/no-variable-string'),
         'required-positional-markers-for-multiple-variables': require('./lib/rules/required-positional-markers-for-multiple-variables'),
+        'has-translation-msgid': require('./lib/rules/has-translation-msgid'),
     },
     rulesConfig: {
         'no-variable-string': 0,
